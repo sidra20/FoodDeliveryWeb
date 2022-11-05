afc36ff6a766af849a516ce78c1eaa64067f0926
Sidra Paracha <sidraparacha27@gmail.com>
2022-11-05 01:02:54 -0700

    initial

--- FoodOrderApplication/Online_food_order/Online_food_order/settings.py
+++ FoodOrderApplication/Online_food_order/Online_food_order/settings.py
@@ -37,8 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'Website',
-    'Dashboard',
+    'website',
+    'dashboard',
 ]
 
 MIDDLEWARE = [
